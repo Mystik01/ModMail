4364a7805fce1641b451736c348d42864f7e4a95
Mystik <65687597+Mystik01@users.noreply.github.com>
2020-12-29 12:39:59 +0000

    install

---
+++ install.bat
@@ -0,0 +1,103 @@
+@echo off
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo * This file will install all the depedencies for the bot, if you agree and you want to  *
+@echo *                              use Modmail press any key                                *
+@echo *****************************************************************************************
+pause>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  10 seconds to start                                  *
+@echo *                                 [# # # # # # # # # #]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  9 seconds to start                                   *
+@echo *                                 [# # # # # # # # # -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  8 seconds to start                                   *
+@echo *                                 [# # # # # # # # - -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  7 seconds to start                                   *
+@echo *                                 [# # # # # # # - - -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  6 seconds to start                                   *
+@echo *                                 [# # # # # # - - - -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  5 seconds to start                                   *
+@echo *                                 [# # # # # - - - - -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  4 seconds to start                                   *
+@echo *                                 [# # # # - - - - - -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  3 seconds to start                                   *
+@echo *                                 [# # # - - - - - - -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  2 seconds to start                                   *
+@echo *                                 [# # - - - - - - - -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  1 seconds to start                                   *
+@echo *                                 [# - - - - - - - - -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                Starting, please wait while dependencies are instaled                  *
+@echo *                                  0 seconds to start                                   *
+@echo *                                 [- - - - - - - - - -]                                 *
+@echo *****************************************************************************************
+timeout /t 1 /nobreak>null
+cls
+@echo *****************************************************************************************
+@echo *                                Modmail by Mystik                                      *
+@echo *                                      Installing                                       *
+@echo *                        This windows will close after instalation                      *
+@echo *****************************************************************************************
+timeout /t 3 /nobreak>null
+npm install
